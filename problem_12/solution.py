0c38cedafa81a962d32b9996fa1b951cebdf49ed
Flávio Caetano <flavio@vieiracaetano.com>
2013-06-13 16:23:44 -0300

    Problem 12 solved

---
+++ problem_12/solution.py
@@ -0,0 +1,34 @@
+__author__ = 'flaviocaetano'
+
+from datetime import datetime
+
+
+def nth_triangle(n):
+    return sum(range(1, n+1))
+
+
+def divisorGenerator(n):
+    sqrt = lambda x: int(x**0.5)
+
+    for i in xrange(1,sqrt(n)+1):
+        if n%i == 0:
+            yield i;
+            yield n/i;
+
+
+if __name__ == '__main__':
+    t0 = datetime.now()
+
+    i = 1
+    result = None
+    div_list = list()
+
+    while len(div_list) < 500:
+        result = nth_triangle(i)
+        div_list = list(divisorGenerator(result))
+
+        i += 1
+
+    t1 = datetime.now() - t0
+
+    print 'result: %s (%ss)' % (result, t1)
