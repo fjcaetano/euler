d57e5a99116515d4687b39efe382acf194996dd3
Flávio Caetano <flavio@vieiracaetano.com>
2013-06-12 23:39:27 -0300

    Problem 4 solved

---
+++ Problem 4/main.py
@@ -0,0 +1,39 @@
+__author__ = 'Flavio'
+
+import datetime
+
+
+def largest_palindrome(*nums):
+    '''
+    '''
+
+    palindrome = -1
+
+    while max(nums) > 100:
+        n1 = max(nums)
+        n2 = min(nums)
+
+        num = n1 * n2
+        num_str = str(num)
+
+        if num_str == num_str[::-1]:
+            if num > palindrome:
+                palindrome = num
+
+        if n2-1 < 100:
+            n1 -=1
+            n2 = n1
+
+        nums = [n1, n2-1]
+
+    return palindrome
+
+
+if __name__ == '__main__':
+    t0 = datetime.datetime.now()
+
+    pal = largest_palindrome(999, 999)
+
+    t1 = datetime.datetime.now() - t0
+
+    print 'result: %d (%ss)' % (pal, t1)
