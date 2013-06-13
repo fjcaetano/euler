68ba137ef2a83f06db88b9b022a6b2d2e6a8d1be
Flávio Caetano <flavio@vieiracaetano.com>
2013-06-13 13:55:05 -0300

    Problem 7 solved

---
+++ problem_7/solution.py
@@ -0,0 +1,39 @@
+__author__ = 'flaviocaetano'
+
+from datetime import datetime
+
+
+def gen_primes(number):
+    """
+    Genereates a list with all primes below the given number
+
+    http://stackoverflow.com/a/568618
+    """
+    composites = {}
+    prime = 2
+    result = []
+
+    while len(result) < number:
+        if prime not in composites:
+            result.append(prime)
+
+            composites[prime * prime] = [prime]
+        else:
+            for p in composites[prime]:
+                composites.setdefault(p + prime, []).append(p)
+
+            del composites[prime]
+
+        prime += 1
+
+    return result.pop()
+
+
+if __name__ == '__main__':
+    t0 = datetime.now()
+
+    result = gen_primes(10001)
+
+    t1 = datetime.now() - t0
+
+    print 'result: %s (%ss)' % (result, t1)
